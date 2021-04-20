b682bec1c890c84e6be813df08fb059be8a8edce
Rohithaditya <70464104+rohithaditya@users.noreply.github.com>
2021-04-20 16:15:37 +0530

    Create cntct.php

---
+++ Contact Page/cntct.php
@@ -0,0 +1,6 @@
+<?php //if "email" variable is filled out, send email   if (isset($_REQUEST['email']))  {      //Email information   $admin_email = "rohithsreedharan2004@gmail.com";   $email = $_REQUEST['email'];   $subject = $_REQUEST['subject'];   $comment = $_REQUEST['comment'];      //send email   mail($admin_email, "$subject", $comment, "From:" . $email);      //Email response   echo "Thank you for contacting us!";   }      //if "email" variable is not filled out, display the form   else  { ?>   <form method="post">
+   Email: <input name="email" type="text" />
+   Subject: <input name="subject" type="text" />
+   Message:
+   <textarea name="comment" rows="15" cols="40"></textarea>
+   <input type="submit" value="Submit" />   </form>    <?php   } ?>
